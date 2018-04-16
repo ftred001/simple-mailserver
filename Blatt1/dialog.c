64aea852dba4e13c4aa2e0054c8a367e6f30440c
Florian Treder <florian.treder@student.hs-rm.de>
2018-04-16 15:44:45 +0200

    Blatt1 findDialogRec

---
+++ Blatt1/dialog.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include "dialog.h"
+
+int validate_noparam() {
+	return 0;
+}
+
+int validate_onoff() {
+	return 1;
+}
+
+DialogRec dialog[] = {
+	/* Command,			Param, 	State,	Next-State,	Validator */
+	{ "BEGIN", 			"",		0,		1,			validate_noparam },
+	{ "kuehlschrank",	"",		1,		1,			validate_onoff },
+	{ "fernseher", 		"", 	1,		1,			validate_onoff },
+	{ "toaster", 		"", 	1,		1},
+	{ "END", 			"", 	1,		2,			validate_noparam },
+	{ "" }
+};
+
+/* Returns DialogRec if found. Else NULL */
+DialogRec *findDialogRec(char *command, DialogRec dialogspec[]) {
+ 	int i;
+	for (i=0; i<CMDMAX; i++) {
+		if (strlen(dialogspec[i].command)) {
+			if (!(strcasecmp(command, dialogspec[i].command))) {
+				return &dialogspec[i];
+			}
+		}
+	}
+	return NULL;
+}
+
+int main(void) {
+	DialogRec *match;
+	
+	match = findDialogRec("toast forever", dialog);
+	if (match !=NULL) {
+			printf("Command: [ %s ] Param: [ %s ]\n",match->command, match->param);
+	}
+
+	
+	return 0;
+}
---
+++ Blatt1/dialog.h
@@ -0,0 +1,42 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+enum { CMDMAX=17, PARAMMAX=80, LINEMAX=100 };
+
+typedef struct drec {
+    char command[CMDMAX];   /* Kommando-String, case-insensitiv */
+    char param[PARAMMAX];   /* Parameter-String */
+    int  state;             /* notwendiger Systemzustand-Level */
+    int  nextstate;         /* Folgezustand nach diesem Kommando */
+    int (*validator)(struct drec *); /* Validatorfktn oder NULL */
+    char is_valid;          /* ist dieses Record ist gueltig? */
+} DialogRec;
+
+/*
+ * findDialogRec() sucht DialogRec zu Kommando 'command' in 
+ * Array 'dialogspec' und liefert Zeiger darauf oder NULL, 
+ * falls nicht gefunden. Das Ende des Arrays 'dialogspec' 
+ * enthaelt den Leerstring in der 'command'-Komponente.
+ */
+DialogRec *findDialogRec(char *command, DialogRec dialogspec[]);
+
+/*
+ * processLine() zerlegt 'line' in einen Kommando- und (optional)
+ * Parameter-Teil und kopiert Parameter in 'param'-Komponente
+ * des zugehoerigen DialogRec im uebergebenen Array 'dialogspec'.
+ * Arrayende ist wieder durch den Leerstring in 'command' markiert.
+ * Ergebnis ist gefuellte ProlResult-struct. 'info' enthaelt eine
+ * optionale Rueckmeldung fuer den Fehlerfall (failed != 0).
+ * In 'state' wird der aktuelle Sytemzustand uebergeben.
+ */
+enum {GRMSGMAX = 120};
+typedef struct pr {
+    char failed;            /* processLine() erfolgreich? */
+    DialogRec *dialogrec;   /* Bezugs-DialogRec */
+    char info[GRMSGMAX];    /* Info-String an Aufrufer */
+} ProlResult;
+
+ProlResult processLine(char line[LINEMAX], int state, DialogRec dialogspec[]);
+
+#endif
+
