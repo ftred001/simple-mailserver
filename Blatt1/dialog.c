be74ce6a920692541487c24ec8bf24e970cf70c9
Florian Treder <florian.treder@student.hs-rm.de>
2018-04-20 16:32:19 +0200

    Blatt 1 fertig

--- Blatt1/dialog.c
+++ Blatt1/dialog.c
@@ -6,11 +6,18 @@
 int globalstate = 0;
 
 int validate_noparam(DialogRec *d) {
-	return !d->param;
+	return !!d->param;
 }
 
 int validate_onoff(DialogRec *d) {
 	return !strcmp(d->param, "on") || !strcmp(d->param, "off"); 
+}
+
+int validator(DialogRec *d) {
+	if (d->validator!=NULL) {
+		return d->validator(d);	
+	}
+	return 1;
 }
 
 DialogRec dialog[] = {
@@ -45,8 +52,10 @@ ProlResult processLine(char line[LINEMAX], int state, DialogRec dialogspec[]) {
 	char *param = line;
 	char infomsg[GRMSGMAX];
 	
+	result.dialogrec = NULL;
+	result.failed = 1;
+	
 	token = __strtok_r(param, divider, &param);
-	printf("Command: %s Param: %s\n", token, param);
 	
 	drecord = findDialogRec(token, dialogspec);
 	if (drecord !=NULL) {
@@ -54,42 +63,51 @@ ProlResult processLine(char line[LINEMAX], int state, DialogRec dialogspec[]) {
 		if (drecord->state == state) {
 			strcpy(drecord->param, param);
 			
+			/* Validate params */
+			if (validator(drecord)) {
+				/* Lineprocessing successful */
+				drecord->is_valid = 1;
+				result.failed = 0;
+				strcpy(result.info, "OK");
+				result.dialogrec = dialogspec;
+				globalstate = drecord->nextstate;
+			} else {
+				drecord->is_valid = 0;
+				strcpy(result.info, "Validation failed\n");
+			}
 		} else {
-			result.failed = '1';
-			result.dialogrec = NULL;
-			strcpy(result.info, "The global state did not drecord the command-state");
+			strcpy(result.info, "The global state did not match the command-state.");
 		}
-		
-		
-		
-		
-		/* Ändere State */
-		globalstate = drecord->nextstate;
-		result.failed = '0';
-		strcpy(result.info, "Command was found");
-		printf("Command: [ %s ] Param: [ %s ]\n",drecord->command, drecord->param);
 	} else {
-		result.failed = '1';
-		result.dialogrec = NULL;
 		strcpy(infomsg, "The following command was not found: ");
 		strcat(infomsg, token);
 		strcpy(result.info, infomsg);
 	}
-	
-
 	return result;
 }
 
+void printRes(ProlResult res) {
+	printf("%d %s | Global-State: %d \n", res.failed, res.info, globalstate);	
+}
 
 int main(void) {
 	ProlResult res;
 	char line[LINEMAX] = "BEGIN";
 	char line2[LINEMAX] = "Toaster forever";
+	char line3[LINEMAX] = "fernseher on";
+	char line4[LINEMAX] = "END";
+	char line5[LINEMAX] = "BEGIN";
 
 	res = processLine(line, globalstate, dialog);
-	printf("Status: %c Info: %s State: %d \n", res.failed, res.info, globalstate);
+	printRes(res);
 	res = processLine(line2, globalstate, dialog);
-	printf("Status: %c Info: %s State: %d \n", res.failed, res.info, globalstate);
+	printRes(res);
+	res = processLine(line3, globalstate, dialog);
+	printRes(res);
+	res = processLine(line4, globalstate, dialog);
+	printRes(res);
+	res = processLine(line5, globalstate, dialog);
+	printRes(res);
 	
 	return 0;
 }
