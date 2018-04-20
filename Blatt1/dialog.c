927b1af7de6467c08800e0f8718c9d4a31a329e2
Florian Treder <florian.treder@student.hs-rm.de>
2018-04-20 15:14:46 +0200

    processLine step1

--- Blatt1/dialog.c
+++ Blatt1/dialog.c
@@ -34,19 +34,35 @@ DialogRec *findDialogRec(char *command, DialogRec dialogspec[]) {
 	return NULL;
 }
 
-/* Aufgabe 2 
+
 ProlResult processLine(char line[LINEMAX], int state, DialogRec dialogspec[]) {
+	ProlResult result;
+	DialogRec *match;
+	char *divider = " ";
+	char *token;
+	char *param = line;
 	
-} */
+	token = __strtok_r(param, divider, &param);
+	printf("Command: %s Param: %s\n", token, param);
+	match = findDialogRec(token, dialogspec);
+	if (match !=NULL) {
+		result.failed = '0';
+		strcpy(result.info, "Command was found");
+		printf("Command: [ %s ] Param: [ %s ]\n",match->command, match->param);
+	} else {
+		result.failed = '1';
+		result.dialogrec = NULL;
+		strcpy(result.info, "Command was not found");
+	}
+	printf("Result ist: %c %s\n", result.failed, result.info);
+	return result;
+}
 
 
 int main(void) {
-	DialogRec *match;
-	
-	match = findDialogRec("toast forever", dialog);
-	if (match !=NULL) {
-			printf("Command: [ %s ] Param: [ %s ]\n",match->command, match->param);
-	}
+	char line[LINEMAX] = "toaster forever";
+
+	processLine(line, 0, dialog);
 
 	
 	return 0;
