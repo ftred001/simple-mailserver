665cb2c809a23a3ebcfead1066d94368a2036846
Florian Treder <florian.treder@student.hs-rm.de>
2018-05-17 15:36:37 +0200

    db_del implemented

--- Blatt3/database.c
+++ Blatt3/database.c
@@ -20,7 +20,7 @@ int speichern(int fd, DBRecord *dbr) {
 
 int match_filter(DBRecord *rec, const void *data) {
 	if (strlen(data) == 0) {
-		return 1;
+		return 2;
 	}
 	
 	if (strstr(rec->key, data)) {
@@ -32,145 +32,146 @@ int match_filter(DBRecord *rec, const void *data) {
 	return 0;
 }
 
-
-int db_search(const char *filepath, int start, DBRecord *record) {
-	/* returns index if successful, returns -1 if not found, -42 if not found */
-	int map_fd, laenge, i;
-	unsigned long data_count;
-	DBRecord *maprecord;
-	
-	map_fd = open(filepath, O_RDWR, 0644);
-	laenge = lseek(map_fd, 0, SEEK_END);
-	
-	data_count = laenge / sizeof(DBRecord);
-	
-	maprecord = mmap(0,laenge, PROT_READ|PROT_WRITE, MAP_SHARED, map_fd, 0);
-	printf("=====SEARCH RESULT Key: %s OR Cat: %s=====\n", record->key, record->cat);
-
-	for (i=0; i<data_count; i++) {
-		if (!strcmp(record->key, maprecord[i].key)) {
-			printf("Key-Match: %d\n\n",i);
-			munmap(maprecord, laenge);
-			close(map_fd);
-			return i;
-		}
-		if (!strcmp(record->cat, maprecord[i].cat)) {
-			printf("Cat-Match: %d\n\n",i);
-			munmap(maprecord, laenge);
-			close(map_fd);
-			return i;
-		}
-	}
-	munmap(maprecord, laenge);
-    close(map_fd);
-	return -1;
-}
-
-int db_get(const char *filepath, int index, DBRecord *result) {
-	/* returns 0 if successful. else -1 */
-	int fd, laenge;
-	DBRecord *maprecord;
-	
-	fd = open(filepath, O_RDWR, 0644);
-	laenge = lseek(fd, 0, SEEK_END);
-	
-	maprecord = mmap(0,laenge, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-	strcpy(result->key, maprecord[index].key);
-	strcpy(result->cat, maprecord[index].cat);
-	strcpy(result->value, maprecord[index].value);
-    
-    close(fd);
-	
-	printf("=====GET Result=====\n%s %s %s\n\n", result->key, result->cat, result->value);
-	return 0;
-}
-
 int db_list(const char *path, int outfd, 
   int (*filter)(DBRecord *rec, const void *data), const void *data) {
-	/* Liest Datenbank ein und schreibt Inhalt als menschenlesbare Tabelle in outfd. */
-	int in_fd, laenge,i,s;
+	/* TODO: Fehlerfälle einarbeiten. */
+	int in_fd, file_length,i,s;
 	int key_rest, cat_rest, value_rest;
 	unsigned long data_count;
-	DBRecord *maprecord;
+	DBRecord *record_map;
 	char *zeile = malloc(sizeof(DBRecord)+ 8);
-	char *separator = " | ";
+	char *separator = "|";
+	int filter_res;
 	zeile  = strcpy(zeile,"");
 	
 	in_fd = open(path, O_RDONLY);
-	laenge = lseek(in_fd,0, SEEK_END);
+	file_length = lseek(in_fd,0, SEEK_END);
 	
-	data_count = laenge / sizeof(DBRecord);
+	data_count = file_length / sizeof(DBRecord);
 	
-	maprecord = mmap(0, laenge, PROT_READ, MAP_SHARED, in_fd, 0);
+	record_map = mmap(0, file_length, PROT_READ, MAP_SHARED, in_fd, 0);
+		
 	
-	
-	/* Format List */
+	/* Format String List */
 	for (i=0; i<data_count; i++) {
 		/* Wenn Filter-Regeln stimmen, dann mache folgendes: */
-		if (filter(&maprecord[i],data)) {
-			zeile = strcat(zeile, "| ");
+		if ((filter_res = filter(&record_map[i],data))) {
+			zeile = strcat(zeile, separator);
 			/* Key */
-			zeile = strcat(zeile, maprecord[i].key);
-			key_rest = DB_KEYLEN - strlen(maprecord[i].key);
+			zeile = strcat(zeile, record_map[i].key);
+			key_rest = DB_KEYLEN - strlen(record_map[i].key);
 			for (s=0; s<key_rest; s++) {
 				zeile = strcat(zeile, " ");
 			}
 			zeile = strcat(zeile, separator);
 			
 			/* Cat */
-			zeile = strcat(zeile, maprecord[i].cat);
-			cat_rest = DB_CATLEN - strlen(maprecord[i].cat);
+			zeile = strcat(zeile, record_map[i].cat);
+			cat_rest = DB_CATLEN - strlen(record_map[i].cat);
 			for (s=0; s<cat_rest; s++) {
 				zeile = strcat(zeile, " ");
 			}
 			zeile = strcat(zeile, separator);
 			
 			/* Value */
-			zeile = strcat(zeile, maprecord[i].value);
-			value_rest = DB_VALLEN - strlen(maprecord[i].value);
+			zeile = strcat(zeile, record_map[i].value);
+			value_rest = DB_VALLEN - strlen(record_map[i].value);
 			for (s=0; s<value_rest; s++) {
-				zeile = strcat(zeile, " ");
+				zeile = strcat(zeile, "");
 			}
-			zeile = strcat(zeile, " |\n");
+			zeile = strcat(zeile, separator);
+			zeile = strcat(zeile, "\n");
 		}
 	}
 	
-	printf("==========DB List:==========\n%s \n", zeile);
+	if (filter_res != 2) {
+		printf("==========DB List==========\n-------Filter: Ja-------\n%s \n", zeile);
+	} else {
+		printf("==========DB List==========\n-------Filter: Nein-------\n%s \n", zeile);
+	}
 	
-	munmap(maprecord, laenge);
+	
+	munmap(record_map, file_length);
     close(in_fd);
     
-	return -42;
+	return -1;
 }
 
-int db_update(const char *filepath, const DBRecord *new) {
-<<<<<<< HEAD
-	/* TODO: IMPLEMENT */
-=======
-    /* fügt in die Datenbank filepath einen neuen Satz new
- hinzu. Existiert bereits ein Satz mit den gleichen Werten für 
-key und cat, so wird dessen value aktualisiert (der Satz wird 
-überschrieben und behält seine Index-Nummer). Existiert die gegebene 
-key/cat-Kombination noch nicht in der Datenbank, so wird ein entsprechender neuer Satz hinten 
-angehängt. Rückgabewert ist in beiden Fällen die Index-Nummer des aktualisierten bzw. 
-hinzugefügten Satzes bzw. -1 im Fehlerfall. */
 
-    
->>>>>>> 9366a02220167c146b4c0d1378994bac5adcd5eb
-	return -42;
+int db_search(const char *filepath, int start, DBRecord *record) {
+	/* TODO: Fehlerfall einarbeiten -> return -42 */
+	int map_fd, file_length, i;
+	unsigned long data_count;
+	DBRecord *record_map;
+	
+	map_fd = open(filepath, O_RDWR, 0644);
+	file_length = lseek(map_fd, 0, SEEK_END);
+	
+	data_count = file_length / sizeof(DBRecord);
+	
+	record_map = mmap(0,file_length, PROT_READ|PROT_WRITE, MAP_SHARED, map_fd, 0);
+	printf("=====SEARCH RESULT Key: %s OR Cat: %s=====\n", record->key, record->cat);
+
+	for (i=0; i<data_count; i++) {
+		if (!strcmp(record->key, record_map[i].key)) {
+			printf("Key-Match in Index: %d\n\n",i);
+			munmap(record_map, file_length);
+			close(map_fd);
+			return i;
+		}
+		if (!strcmp(record->cat, record_map[i].cat)) {
+			printf("Cat-Match in Index: %d\n\n",i);
+			munmap(record_map, file_length);
+			close(map_fd);
+			return i;
+		}
+	}
+	printf("Error: No Match for Key OR Cat\n\n");
+	munmap(record_map, file_length);
+    close(map_fd);
+	return -1;
+}
+
+int db_get(const char *filepath, int index, DBRecord *result) {
+	int fd, file_length;
+	unsigned long data_count;
+	DBRecord *record_map;
+	
+	fd = open(filepath, O_RDWR, 0644);
+	file_length = lseek(fd, 0, SEEK_END);
+	data_count = file_length / sizeof(DBRecord);
+	
+	/* Index > als Anzahl Datensätze */
+	if (index>=data_count) {
+		close(fd);
+		printf("=====GET Result Index: %d=====\nERROR: Index out of Bounds\n\n", index);
+		return -1;
+	}
+	
+	/* Kopiere Werte in den Result-Pointer */
+	record_map = mmap(0,file_length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	strcpy(result->key, record_map[index].key);
+	strcpy(result->cat, record_map[index].cat);
+	strcpy(result->value, record_map[index].value);
+	
+	munmap(record_map, file_length);
+    close(fd);
+	
+	printf("=====GET Result Index: %d=====\n%s %s %s\n\n", index, result->key, result->cat, result->value);
+	return 0;
 }
 
 int db_put(const char *filepath, int index, const DBRecord *record) {
-	int fd, laenge;
-	DBRecord *maprecord;
+	int fd, file_length;
+	DBRecord *record_map;
     unsigned long data_count;
 	
-	printf("========PUT OPERATION========\n PUT RECORD %s in Index: %d \n\n", record->key, index);
+	printf("========PUT OPERATION========\nPUT RECORD %s in Index: %d \n\n", record->key, index);
 	
 	fd = open(filepath, O_RDWR, 0644);
-	laenge = lseek(fd, 0, SEEK_END);
+	file_length = lseek(fd, 0, SEEK_END);
 	
-    data_count = laenge/sizeof(DBRecord);
+    data_count = file_length/sizeof(DBRecord);
     
     
     if (index >= data_count || index < 0) {
@@ -180,20 +181,88 @@ int db_put(const char *filepath, int index, const DBRecord *record) {
         return 1;
     }
     
-	maprecord = mmap(0,laenge, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	record_map = mmap(0,file_length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 	
-	strcpy(maprecord[index].key, record->key);
-	strcpy(maprecord[index].cat, record->cat);
-	strcpy(maprecord[index].value,record->value);
+	strcpy(record_map[index].key, record->key);
+	strcpy(record_map[index].cat, record->cat);
+	strcpy(record_map[index].value,record->value);
 	
-	munmap(maprecord, laenge);
+	munmap(record_map, file_length);
 	close(fd);
 	return 1;
 }
 
+int db_update(const char *filepath, const DBRecord *new) {
+	int search_result, index=-1;
+	DBRecord *copy = calloc(0,sizeof(DBRecord));
+	
+	strcpy(copy->key, new->key);
+	strcpy(copy->cat, new->cat);
+	strcpy(copy->value, new->value);
+	
+	
+    search_result = db_search(filepath, 0, copy);
+    
+    if (search_result != 42) {
+		db_put(filepath, search_result, copy);
+			
+	}
+        
+	return index;
+}
+
+
+
 int db_del(const char *filepath, int index) {
-	/* TODO: IMPLEMENT */
-	return -42;
+	int i, fd, file_length, cache_fd;
+	unsigned long data_count;
+	char *cachepath = "cachedatei";
+
+	DBRecord *record_map;
+	DBRecord *copy_rec = calloc(0, sizeof(DBRecord));
+
+	
+	printf("========DELETE OPERATION========\nDELETE RECORD at Index: %d \n\n", index);
+	
+	fd = open(filepath, O_RDWR, 0644);
+	file_length = lseek(fd, 0, SEEK_END);
+    data_count = file_length/sizeof(DBRecord);
+    
+    
+    if (index >= data_count || index < 0) {
+        printf("Index-Error DataCount: %ld Geforderter Index: %d\n", data_count, index);
+        close(fd);
+        return -1;
+    }
+    
+    /* Lege Cache-Datei an */
+    cache_fd = open(cachepath, O_WRONLY|O_TRUNC|O_CREAT,0644);
+    
+    /* Kopiere alle Records außer Index */
+	record_map = mmap(0,file_length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	
+	for (i=0; i<data_count; i++) {
+		if (index!=i) {
+			strcpy(copy_rec->key, record_map[i].key);
+			strcpy(copy_rec->cat, record_map[i].cat);
+			strcpy(copy_rec->value, record_map[i].value);
+			if (write(cache_fd, copy_rec, sizeof(DBRecord)) < 0 ) {
+				perror("speichern (write)\n");
+				return -1;
+			}
+		}
+
+	}
+	munmap(record_map, file_length);
+	close(fd);
+
+
+	/* Benenne Cache-Datei in vorherigen Namen um */
+	close(cache_fd);
+	rename(cachepath,filepath);
+	
+
+	return 1;
 }
 
 int main(int argc, char *argv[]) {
@@ -203,8 +272,11 @@ int main(int argc, char *argv[]) {
 	DBRecord test2 = {"key2", "Kategorie 2", "Values sind wirklich super!"};
 	DBRecord test3= {"key3", "cat3", "value toll"};
 	DBRecord test4 = {"key4", "cat4", "Values sind wirklich super!"};
-	DBRecord such_record = {"key4", "Kategoriename", "Toller Value"};
+	DBRecord such_treffer = {"key4", "Kategoriename", "Toller Value"};
+	DBRecord such_niete = {"Schlüssel", "Wursti", "Toller Value"};
 	DBRecord replace_record = {"key_ersatz", "Kategorieersatz", "Neuer Wert"};
+	const DBRecord update_record = {"key1", "cat1", "Dieser Value ist noch viel toller als der vorherige"};
+	
 	DBRecord index_result;
 	
 	
@@ -223,15 +295,24 @@ int main(int argc, char *argv[]) {
 	close(schreib_fd);
 	/* ENDE Schreibe Testdaten in Datei ENDE*/
 	
+	db_get(filepath, 17, &index_result);
 	
-	search_result = db_search(filepath, 0, &such_record);
+	search_result = db_search(filepath, 0, &such_treffer);
+	search_result = db_search(filepath, 0, &such_niete);
+	
 	db_get(filepath, search_result, &index_result);
 
-	db_list(filepath, schreib_fd, match_filter,"");
+	db_list(filepath, schreib_fd, match_filter,"key");
 	
 
 	db_put(filepath, -1, &replace_record);
 	db_list(filepath, schreib_fd, match_filter,"");
+	
+	db_update(filepath, &update_record);
+	db_list(filepath, schreib_fd, match_filter, "");
+	
+	db_del(filepath, 5);
+	db_list(filepath, schreib_fd, match_filter, "");
 	
 	return 0;
 }
