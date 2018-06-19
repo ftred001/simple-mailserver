d41052346342b0434accb4355dc8f6aea47e2850
Florian Treder <florian.treder@student.hs-rm.de>
2018-06-19 16:14:12 +0200

    Blöde Liste will nicht

--- Blatt4/fileindex.c
+++ Blatt4/fileindex.c
@@ -24,28 +24,86 @@
  * ACHTUNG: Leerzeilen gehören zum Inhalt.
  *  */
  
+ void print_entry(FileIndexEntry *e) {
+	printf("----Entry #%d----\n", e->nr);
+	printf("Seekpos: %d\n", e->seekpos);
+	printf("Size: %d\n", e->size);
+	printf("Lines: %d\n", e->lines);
+	printf("DEL_FLAG %d\n", e->del_flag);
+	if (e->next == NULL) {
+		printf("NEXT -> NULL\n");
+	} else {
+		printf("NEXT -> NOT NULL\n");
+	}
+	printf("\n\n");
+}
+
+void print_fi(FileIndex *i) {
+	printf("----FileIndex----\n");
+	printf("Filepath: %s\n", i->filepath);
+	printf("nEntries: %d\n", i->nEntries);
+	printf("totalSize: %d\n", i->totalSize);
+	printf("\n\n");
+}
+ 
  FileIndexEntry *new_entry(int nr) {
-	FileIndexEntry *entry = calloc(sizeof(FileIndexEntry), 0);
+	FileIndexEntry *entry = calloc(1, sizeof(FileIndexEntry));
+	
 	entry->next = NULL;
 	entry->nr = nr;
+	
+	printf("New Entry #%d\n", entry->nr);
+	
 	return entry;
 }
 
-FileIndexEntry *append_entry(FileIndexEntry *parent, FileIndexEntry *child) {
-	parent->next = child;
-	return child;
+ void print_entries(FileIndex *fi) {
+	FileIndexEntry *e = NULL;
+	
+	e = fi->entries;
+	
+	while(e->next != NULL) {
+		print_entry(e);
+		e = e->next;
+	}
 }
+
+void append_entry(FileIndex *i, FileIndexEntry *e)  {
+	FileIndexEntry *ptr;
+	
+	printf("APPEND ENTRY\n");
+	
+	if (i->entries == NULL) {
+		printf("Entries = NULL\n");
+		i->entries = e;
+		return;
+	}
+	
+	printf("ENTRIES NOT NULL\n");
+	
+	ptr = i->entries;
+	
+	while(ptr->next != NULL) {
+		ptr = ptr->next;
+	}
+	
+	ptr->next = e;
+	return;
+}
+ 
+ 
+
  
 /* Indiziert angegebene Datei filepath. Trennzeilen sind wie SEPARATOR und gehören NICHT zum Inhalt
  * return Zeiger auf FileIndex wenn okay
  * return NULL bei Fehler
  * */
 FileIndex *fi_new(const char *filepath, const char *separator) {
-	FileIndex *findex = calloc(sizeof(FileIndex), 0);
-	FileIndexEntry *entry, *mem;
+	FileIndex *findex = calloc(1, sizeof(FileIndex));
+	FileIndexEntry *entry, *ptr;
 	char *line = calloc(1024, sizeof(char));
 	LineBuffer *b;
-	int fd, umbruch=0, offsetpos=0;
+	int fd, umbruch=0;
 	int is_head=0;
 	
 	
@@ -58,47 +116,44 @@ FileIndex *fi_new(const char *filepath, const char *separator) {
 	/* FIEntry für jeden Abschnitt. */
 	while ((umbruch = buf_readline(b, line, LINEBUFFERSIZE)) !=-1) {
 		 if (umbruch >= 0) {
-			findex->totalSize += umbruch + b->lineseplen; 
+			findex->totalSize =  findex->totalSize + umbruch + b->lineseplen; 
 			
 			/* Sektionsanfang */
 			if (!strncmp(line, "From ", 5)) {
 				findex->nEntries++;
-				
-				if (!findex->nEntries) {
-					printf("INIT HEAD\n");
-					entry = new_entry(findex->nEntries);
-					findex->entries = entry;
-				} else {
-					printf("APPEND SECTION\n");
-					/* Merke alten Entry */
-					/* Erstelle neuen Entry */
-					/* Lass alten Entry auf neuen Entry zeigen */
-					
-					mem = entry;
-					entry = new_entry(findex->nEntries);
-					append_entry(mem, entry);
-				}
 				is_head = 1;
-				printf("%d ", buf_where(b));
-				printf("ENTRY #%d Size: %d Lines %d\n",entry->nr,entry->size,entry->lines);
+				
+				
+				if (!findex->entries) {
+					findex->entries = new_entry(findex->nEntries);
+					entry = findex->entries;
+					print_entry(entry);
+				} else {
+					while(ptr->next != NULL) {
+						print_entry(ptr);
+						ptr = ptr->next;
+					}
+					ptr->next = new_entry(findex->nEntries);
+					entry = ptr->next;
+					
+				}
+				
 			}
 			
 			if (entry->nr && is_head == 0) {
 				entry->lines++;
 				entry->size += strlen(line) + b->lineseplen;
+				
 			}
 			
 			/* Erste Leerzeile finden -> HEAD Ende */
 			if (!strcmp(line, "") && is_head) {
-				printf("%d LEER\n", buf_where(b));
 				entry->seekpos = buf_where(b)+b->lineseplen;
 				is_head = 0;
 			}
 			 
 		}
 	} 
-	
-	printf("ENTRY #%d Size: %d Lines %d\n",entry->nr,entry->size,entry->lines);
 
 	
 	buf_dispose(b);
@@ -128,6 +183,7 @@ FileIndexEntry *fi_find(FileIndex *fi, int n);
 int fi_compactify(FileIndex *fi);
 
 
+
 int main(int argc, char *argv[]) {
 	FileIndex *findex;
 	const char *line_separator = "\n";
@@ -136,8 +192,8 @@ int main(int argc, char *argv[]) {
 	printf("Lese folgende Datei ein: %s\n\n", argv[1]);
 	
 	findex = fi_new(argv[1], line_separator);
-	
-	
+
+	print_entries(findex);
 	
 	return 0;
 }
